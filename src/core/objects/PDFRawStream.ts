dcda74d6a63c75dd9ac6ef743045006909344fc6
Sandeep Jadhav <sandeepdj11@gmail.com>
2023-11-03 12:41:03 +0530

    Initial commit

---
+++ src/core/objects/PDFRawStream.ts
@@ -0,0 +1,42 @@
+import PDFDict from './PDFDict';
+import PDFStream from './PDFStream';
+import PDFContext from '../PDFContext';
+import { arrayAsString } from '../../utils';
+
+class PDFRawStream extends PDFStream {
+  static of = (dict: PDFDict, contents: Uint8Array) =>
+    new PDFRawStream(dict, contents);
+
+  contents: Uint8Array;
+
+  private constructor(dict: PDFDict, contents: Uint8Array) {
+    super(dict);
+    this.contents = contents;
+  }
+
+  asUint8Array(): Uint8Array {
+    return this.contents.slice();
+  }
+
+  clone(context?: PDFContext): PDFRawStream {
+    return PDFRawStream.of(this.dict.clone(context), this.contents.slice());
+  }
+
+  getContentsString(): string {
+    return arrayAsString(this.contents);
+  }
+
+  updateContent(encrypt: Uint8Array): void {
+    this.contents = encrypt;
+  }
+
+  getContents(): Uint8Array {
+    return this.contents;
+  }
+
+  getContentsSize(): number {
+    return this.contents.length;
+  }
+}
+
+export default PDFRawStream;
